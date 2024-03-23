1824567bdc3f833c8e0eeb23eb3ff28a31b50f23
Andre Kohli <andrek@student.unimelb.edu.au>
2024-03-23 13:54:51 +1100

    new spam

--- Submissions/Bot1.py
+++ Submissions/Bot1.py
@@ -6,6 +6,7 @@ from Game.playerActions import defense_actions, attack_actions, projectile_actio
 from Game.gameSettings import HP, LEFTBORDER, RIGHTBORDER, LEFTSTART, RIGHTSTART, PARRYSTUN
 
 
+
 # PRIMARY CAN BE: Teleport, Super Saiyan, Meditate, Dash Attack, Uppercut, One Punch
 # SECONDARY CAN BE : Hadoken, Grenade, Boomerang, Bear Trap
 
@@ -41,28 +42,28 @@ class Script:
     def __init__(self):
         self.primary = PRIMARY_SKILL
         self.secondary = SECONDARY_SKILL
+        self.comboList = [LIGHT, LIGHT, HEAVY, PRIMARY_SKILL, SECONDARY_SKILL]
+        self.moves_used = []
         
     # DO NOT TOUCH
     def init_player_skills(self):
         return self.primary, self.secondary
     
+    
     # MAIN FUNCTION that returns a single move to the game manager
     def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
         distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
+        
+        while self.comboList:
+            move = self.comboList[0]
+            self.comboList = self.comboList[1:]
+            self.moves_used.append(move)
+            return FORWARD
 
-        comboList = [LIGHT, LIGHT, HEAVY, TeleportSkill, Hadoken]
-        moves_used = []
-
-        while comboList:
-            move = comboList[0]
-            comboList = comboList[1:]
-            moves_used.append(move)
-
-
-        if not comboList:
+        if not self.comboList:
             print("ComboList is empty. Adding moves back.")
-            comboList.extend(moves_used)
-            moves_used.clear()
+            self.comboList.extend(self.moves_used)
+            self.moves_used.clear()
 
         
                 
