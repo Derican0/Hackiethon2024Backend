af8f13e24f8ae074a9a56d6668ab92c8d0a0c73d
Eric Zhang <zhangeric.zhang@gmail.com>
2024-03-23 12:54:29 +1100

    AndreBot1

--- Submissions/template.py
+++ Submissions/template.py
@@ -48,9 +48,14 @@ class Script:
     # MAIN FUNCTION that returns a single move to the game manager
     def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
         distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
-        if not primary_on_cooldown(player) and distance <= prim_range(player):
-            return PRIMARY_SKILL
-        elif not secondary_on_cooldown(player) and distance <= seco_range(player):
-            return SECONDARY_SKILL
-        elif not heavy_on_cooldown(player) and distance <= 1:
-            return HEAVY
+        projectile_distance = 0
+        if len(enemy_projectiles) > 1:
+            projectile_distance = abs(get_pos(player)[0] - get_pos(enemy_projectiles[0]))
+
+        if not secondary_on_cooldown(player) and distance <= seco_range(player):
+            return SECONDARY
+        elif projectile_distance == 1:
+            return BLOCK
+        else:
+            return FORWARD
+        
