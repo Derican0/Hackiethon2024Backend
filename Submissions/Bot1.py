0d90f076dc274282f8bb3b6da0fbbe8dd5d0e350
Eric Zhang <zhangeric.zhang@gmail.com>
2024-03-23 12:28:05 +1100

    fixed enemy projectile

--- Submissions/Bot1.py
+++ Submissions/Bot1.py
@@ -49,7 +49,9 @@ class Script:
     # MAIN FUNCTION that returns a single move to the game manager
     def get_move(self, player, enemy, player_projectiles, enemy_projectiles):
         distance = abs(get_pos(player)[0] - get_pos(enemy)[0])
-        projectile_distance = abs(get_pos(player)[0] - get_pos(enemy_projectiles[1]))
+        if len(enemy_projectiles) > 1:
+            projectile_distance = abs(get_pos(player)[0] - get_pos(enemy_projectiles[0]))
+            
         if not secondary_on_cooldown(player) and distance <= seco_range(player):
             return SECONDARY
         elif projectile_distance == 1:
